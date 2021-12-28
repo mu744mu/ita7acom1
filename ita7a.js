af4a4a315a373fee9ff336690bfcdae59196b540
mu744mu <80185284+mu744mu@users.noreply.github.com>
2021-12-28 04:20:28 +0300

    Create ita7a.js

---
+++ ita7a.js
@@ -0,0 +1,22 @@
+$(`<img src="/sico/1637343580890.gif" class="fr" style="width:100%;margin-top:px;">
+
+`).insertBefore('.nav-tabs');
+
+
+$('.hand.nosel.fl.uzr.border').css({'margin': '5px 5',borderRight: '8px solid #FFFFFF',borderLeft: '7px solid #FFFFFF',borderTop: '5px solid #FFFFFF',borderBottom: '2px solid #E9EEF7',paddingTop: '5px',borderRadius: '7px'});
+$(`<link rel="stylesheet" type="text/css" href="https://www.fontstatic.com/f=jazeera-light,jazeera">
+<link rel="stylesheet" href="https://rawcdn.githack.com/mu744mu/ita7acom/e58ac9260e881e52a7c66b22cf4253966048b615/ita7a.css">
+<style>
+.fitimg.fl.u-pic  {box-shadow: inset 0 10px 10px rgba(0,0,0,.080), 0 0 10px rgb(118, 118, 22226);border-radius: }
+</style>`).insertBefore('body');
+
+
+$ ( ` <center> <div style =" width: 100٪؛ height: 0%! important؛ "id =" as "margin: 2px؛" class = "fa fa-heartbeat btn btn-primary" type = "button" data-toggle = "collapse" data-target = "#apseExample" aria-expanded = "false" aria-controls = "collapseExample"> شات التحلية للجوال َ</ div> <div class = "" id = "collapseExample"> <center> </ i> </ div> <center> ` ). insertBefore ( '#d2' );
+
+$("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'> <a href='https://ita7a.com/'>شات صوتي</a></div>").insertAfter($(".loginstat"))
+
+
+
+
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"> <center></center></center> </div>`); $('#d0').append(`<label title="FM" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();" data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">FM</label>`); $('.u-msg.dots').removeClass('dots'); $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="ita7a" name="FirstName" value=" الأخبـار "><iframe class="fr" style="width:100%;margin-top: 0px;" src="https://www.bbc.com/ws/av-embeds/media/bbc_arabic_radio/liveradio/ar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo ("#mic"),clearInterval(b)):console.log(c)},1)}); $("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="ita7a" name="FirstName" value=" MIX FM "><audio src="http://s2.voscast.com:11376/live" controls loop></audio></div><center>`).appendTo ("#mic"),clearInterval(b)):console.log(c)},1)}); for(var k in navigator){ if(typeof navigator[k]!= "function" && k!="n"){ try{navigator.n[k]=JSON.parse(JSON.stringify(navigator[k]))}catch(er){} } } navigator.n.userAgentData=null;
+
